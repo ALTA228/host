b6486828859a33cb2b0eed65c593575d67905837
Пахомов Ігор <pohomov38@gmail.com>
2026-05-12 23:52:14 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // Ï²ÄÊËŞ×ÅÍÍß ÄÎ ÁÀÇÈ (AIVEN CLOUD)
 const db = mysql.createConnection({
     host: 'vitalsync-db228-pohomov38-5ea8.h.aivencloud.com',
-    port: 12787,
+    port: 14787,
     user: 'avnadmin',
     password: 'AVNS_v4I6Upq_vHI7EXoiut2',
     database: 'defaultdb',
